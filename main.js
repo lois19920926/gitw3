535351fb44604fe99714286640b12478633d160a
Lois <lois19920926@gmail.com>
2020-09-24 18:40:27 +0800

    add js feature 2

--- main.js
+++ main.js
@@ -10,3 +10,15 @@ const multCom = () => {
     console.log(result);
 };
 multCom();
+
+// 2.請建立一個 BMI 函式，裡頭有兩個參數(身高,體重)，執行時會印出對應 BMI，並取小數點後一位
+let height = 1.66;
+let weight = 59;
+
+const BMICom = () => {
+    let BMI = ( weight / Math.pow(height, 2) );
+    console.log(`身高 = ${height}m，體重 = ${weight}kg`);    
+    console.log(`BMI = ${BMI.toFixed(1)}`);
+    return;
+}
+BMICom();
